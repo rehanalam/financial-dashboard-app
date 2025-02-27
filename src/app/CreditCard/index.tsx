d8308bb8b0141649f51f7f600e35c2497b3adf6f
Muhammad Rehan Alam <rehan@beebolt.com>
2025-02-27 19:19:04 +0500

    improves card UI

--- src/app/CreditCard/index.tsx
+++ src/app/CreditCard/index.tsx
@@ -1,6 +1,9 @@
 import Card from '@/components/Card';
 import React from 'react';
 
+import MasterLogoDark from '../../assets/icons/master-cc-dark-icon.svg?react';
+import MasterLogoLight from '../../assets/icons/master-cc-light-icon.svg?react';
+
 interface CreditCardProps {
   balance: string;
   cardHolder: string;
@@ -9,16 +12,19 @@ interface CreditCardProps {
   theme: 'light' | 'dark';
 }
 
+const computeTheme = (theme: string) => theme === 'dark';
+
 const CreditCard: React.FC<CreditCardProps> = ({ balance, cardHolder, validThru, cardNumber, theme }) => {
-  // Theme-specific styles
-  const themeStyles =
-    theme === 'dark' ? 'bg-gray-800 text-white bg-cc-gradient' : 'bg-white text-blue-800 border border-gray-100';
-  const labelStyles = theme === 'dark' ? 'text-white' : 'text-blue-400';
-  const valueStyles = theme === 'dark' ? 'text-white' : 'text-blue-800';
+  const isDarkTheme = computeTheme(theme);
+  const themeStyles = isDarkTheme
+    ? 'bg-gray-800 text-white bg-cc-gradient'
+    : 'bg-white text-blue-800 border border-gray-100';
+  const labelStyles = isDarkTheme ? 'text-white' : 'text-blue-400';
+  const valueStyles = isDarkTheme ? 'text-white' : 'text-blue-800';
 
   return (
     <Card className={themeStyles}>
-      <div className="space-y-7">
+      <div className="font-lato space-y-7">
         {/* Balance */}
         <div>
           <p className={`text-sm ${labelStyles}`}>Balance</p>
@@ -40,8 +46,11 @@ const CreditCard: React.FC<CreditCardProps> = ({ balance, cardHolder, validThru,
         </div>
 
         {/* Card Number */}
-        <div className={`bg-cc-gradient-footer -mx-6 px-6 pt-4 ${theme !== 'dark' && 'border-t-1 border-gray-100'} `}>
+        <div
+          className={`bg-cc-gradient-footer -mx-6 flex justify-between px-6 pt-5 ${!isDarkTheme && 'border-t-1 border-gray-100'} `}
+        >
           <p className={`text-2xl font-medium ${valueStyles}`}>{cardNumber}</p>
+          {isDarkTheme ? <MasterLogoLight /> : <MasterLogoDark />}
         </div>
       </div>
     </Card>
